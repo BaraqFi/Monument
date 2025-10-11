35e3eb7f609deac71ff7cdc3169236377f8d0723
BaraqFi <mbarrack01@gmail.com>
2025-10-11 12:43:48 -0400

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
@@ -83,6 +84,7 @@ html {
         {children}
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
